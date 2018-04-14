f91e245cf924f9751c3a731a06fd7eb1ca7b640c
João André Pestre <joaoandre.pestre@poli.ufrj.br>
2018-04-14 19:02:01 -0300

    Adiciona funções para resolução de sistemas lineares e minimos quadrados

--- Trab1/main.py
+++ Trab1/main.py
@@ -4,8 +4,39 @@ from GeradorDeMatriz import cria_matriz
 from Matriz import Matriz
 from MatrizQuadrada import MatrizQuadrada
 
+def resolve(matriz, vetor):
+    """Resolve o sistema por decomposição LU"""
 
-LU_ex = cria_matriz([[1, 2, 2],
+    if not isinstance(matriz, MatrizQuadrada):
+        raise ValueError("A matriz deve ser quadrada.")
+
+    (L,U) = matriz.LU(True)
+    resp = L.substituicao_para_frente(vetor)
+    resp = U.retro_substituicao(resp)
+
+    return resp
+
+def minimos_quadrados(pontos):
+    """Retorna os parametros a e b da reta
+    que melhor se ajusta ao conjunto de pontos"""
+    
+    N = len(pontos)
+    P = [[1.0 for j in range(2)] for i in range(N)]
+    for i in range(N):
+        P[i][1] = pontos[i][0]
+    P = Matriz(P)
+    Pt = P.transposta()
+    A = Pt*P
+    A = cria_matriz(A.mat)
+
+    y = [pontos[i][1] for i in range(N)]
+    C = Pt*y
+    
+    resp = resolve(A,C)
+    return (resp[0],resp[1])
+
+
+""" LU_ex = cria_matriz([[1, 2, 2],
                      [4, 4, 2],
                      [4, 6, 4]])
 
@@ -41,6 +72,9 @@ A = cria_matriz([[1.0, 0.2, 0.0],
                  [0.0, 0.5, 1.0]])
 
 (a, v) = A.Jacobi()
-print("\nAutovalor:\n"+str(a)+"Autovetor:\n"+str(v))
+print("\nAutovalor:\n"+str(a)+"Autovetor:\n"+str(v)) """
+
+(b,a) = minimos_quadrados([(1.0,2.0),(2.0,3.5),(3.0,6.5)])
+print("y = "+str(b)+" + "+str(a)+"x")
 
 print('fim do programa')
